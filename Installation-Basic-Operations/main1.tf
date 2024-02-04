5763721cc372865282adbd82b9d512cce7615887
jonas034 <jonasberg034@gmail.com>
2024-02-04 22:56:36 +0000

    files uploaded

---
+++ Installation-Basic-Operations/main1.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = "ami-0277155c3f0ab2930"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "created-by-tf"
+  }
+}
+
